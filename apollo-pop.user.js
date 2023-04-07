f44979d6900a2d30cc06fc85ae2465f8791b5040
Dario Cruz <rio2012@gmail.com>
2023-04-07 09:12:23 -0400

    Mod some values to be correct, Add placeholder text for username and details.

--- apollo-pop.user.js
+++ apollo-pop.user.js
@@ -12,7 +12,7 @@
     // Alert user to auto pop
     alert('Auto-Pop In Progress!')
 
-    document.querySelector('span.slider.round').click()
+    // document.querySelector('span.slider.round').click()
 
     // Set the values of the form elements
     document.getElementById('audit_properties_Select Sort').value = 'MOR';
@@ -21,22 +21,24 @@
     document.getElementById('audit_properties_Is AA observed using unauthorized electronic devices (phone/earbuds)').value = 'No'
     document.getElementById('audit_properties_Is the associate exhibiting safe lifting/lowering practices (back upright - testing package before lifting - working in power zone - standing close to object -bending/lifting with legs NOT BACK - feet shoulder width apart - hands free)').value = 'Yes'
     document.getElementById('audit_properties_Is the AA handling objects properly (For 1 handed grasping fingers form “C” shape, not “L” shape, hands free and clear, wrist in the neutral position – handshaking position)').value = 'Yes'
-    document.getElementById('audit_properties_Is the AA pivoting at the waist to complete tasks or are they keeping NOSE OVER TOES and picking up and moving feet in the direction of work').value = 'Yes'
-    document.getElementById('audit_properties_Is the AA demonstrating safe and standard work practices for pushing or pulling').value = 'Yes'
-    document.getElementById('audit_properties_Is the AA observed bending at the knees and not the waist to complete tasks below their power zone').value = 'Yes'
+    document.getElementById('audit_properties_Is the AA pivoting at the waist to complete tasks or are they keeping NOSE OVER TOES and picking up and moving feet in the direction of work').value = 'Yes '
+    document.getElementById('audit_properties_Is the AA demonstrating safe and standard work practices for pushing or pulling').value = 'Yes '
+    document.getElementById('audit_properties_Is the AA observed bending at the knees and not the waist to complete tasks below their power zone').value = 'Yes '
     document.getElementById('audit_properties_Have you (the auditor) watched the Working Well Huddle material for the week').value = 'Yes'
-    document.getElementById('audit_properties_Is the AA observed bending at the knees and not the waist to complete tasks below their power zone').value = 'Yes'
-    
+    document.getElementById('audit_properties_Is the AA observed bending at the knees and not the waist to complete tasks below their power zone').value = 'Yes '
+
+    // Make it obvious to the user that they need to enter a username and some details
+    document.getElementById('apollo-employee-login').value = '⚠️⚠️⚠️ENTER A USERNAME⚠️⚠️⚠️'
+    document.getElementById('audit_properties_After observing the AA, have a conversation with them about behaviors observed.  Please list below the conversation key points (both positive and negative)').value = '⚠️⚠️⚠️ENTER SOME DETAILS⚠️⚠️⚠️'
 
     // Simulate click for all checkbox elements.
-
     let spanArray = document.querySelectorAll('span')
 
     spanArray.forEach(element => {
         switch(element.innerText) {
-            case 'Scanner Harness':
-                element.click()
-                break
+            // case 'Scanner Harness':
+            //     element.click()
+            //     break
             case 'Gloves':
                 element.click()
                 break
